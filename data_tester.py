03164186e35ab62d15cec461cfcae4e500f36054
doomed51 <doomed51@gmail.com>
2024-11-30 10:32:13 -0500

    init

--- data_tester.py
+++ data_tester.py
@@ -4,50 +4,46 @@ import matplotlib.pyplot as plt
 
 conn = duckdb.connect('data/options_data.db')
 
-# get tables in db 
-tables = conn.execute('SHOW TABLES').fetchdf()
-print(tables)
+# # List tables in db 
+# tables = conn.execute('SHOW TABLES').fetchdf()
+# print(tables['name'])
+# print('\n')
+
+# ## Main options history table(s)
+# read_query = 'SELECT * FROM options_historical_data'
+# df = conn.execute(read_query).fetchdf()
+# print('options_historical_data Table')
+# print(df.columns)
+
+# underlying table 
 print('\n')
-
-
-read_query = 'SELECT * FROM options_historical_data'
+read_query = 'SELECT * FROM underlying_price_history'
 df = conn.execute(read_query).fetchdf()
 print(df)
 
-# from options hist data select where strike = 602.0, expiry=2024-11-27, type=call
-# read_query = 'SELECT * FROM options_historical_data WHERE strike = 602.0 AND expiry = \'2024-11-27\' AND "right" = \'C\''
-# df = conn.execute(read_query).fetchdf()
-# print(df)
-
-
+## Collection Progress Table
 print('\n')
-# underlying table 
-read_query = 'SELECT * FROM underlying_price_history'
-
-# print SPY-587.0C-20241129
-# read_query = 'SELECT * FROM underlying_price_history WHERE symbol = \'SPY\' AND expiry = \'2024-11-29\' AND strike = 587.0 AND "right" = \'C\''
-# df = conn.execute(read_query).fetchdf()
-# print(df)
-
-print('\n')
+# drop from collection_progress where batch_id = bfcc3d38b559a73d0e20eef98d69838d
+delete_query = 'DELETE FROM collection_progress WHERE batch_id = \'a9220cbd8217ebd5ef232f29e026e10e\''
+conn.execute(delete_query)
 read_query = 'SELECT * FROM collection_progress'
 df = conn.execute(read_query).fetchdf()
+print(df[['batch_id', 'symbol', 'start_date', 'end_date', 'status']])
 
-print(df[['symbol', 'start_date', 'end_date', 'status']])
 
-# print num completed, started, in_progress
-print('\n')
-read_query = 'SELECT COUNT(*) FROM collection_progress WHERE status = \'COMPLETED\''
-df = conn.execute(read_query).fetchdf()
-print(f'completed: {df}')
+# # print num completed, started, in_progress
+# print('\n')
+# read_query = 'SELECT COUNT(*) FROM collection_progress WHERE status = \'COMPLETED\''
+# df = conn.execute(read_query).fetchdf()
+# print(f'completed: {df}')
 
-read_query = 'SELECT COUNT(*) FROM collection_progress WHERE status = \'STARTED\''
-df = conn.execute(read_query).fetchdf()
-print(f'started: {df}')
+# read_query = 'SELECT COUNT(*) FROM collection_progress WHERE status = \'STARTED\''
+# df = conn.execute(read_query).fetchdf()
+# print(f'started: {df}')
 
-read_query = 'SELECT COUNT(*) FROM collection_progress WHERE status = \'IN_PROGRESS\''
-df = conn.execute(read_query).fetchdf()
-print(f'in_progress: {df}')
+# read_query = 'SELECT COUNT(*) FROM collection_progress WHERE status = \'IN_PROGRESS\''
+# df = conn.execute(read_query).fetchdf()
+# print(f'in_progress: {df}')
 
 
 
